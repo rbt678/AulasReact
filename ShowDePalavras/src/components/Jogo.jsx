ed514aee1ec88cbb4e0694af705bb5864f83c69c
Roberto Javier Lagger <javierlaggerr@gmail.com>
2023-09-26 06:04:29 -0300

    a

--- ShowDePalavras/src/components/Jogo.jsx
+++ ShowDePalavras/src/components/Jogo.jsx
@@ -14,6 +14,7 @@ const Jogo = () => {
     const [palavra_na_tela, setPalavra_na_tela] = useState(Array(palavra_array.length).fill(' '));
     const [pontuacaoAtual, setPontuacaoAtual] = useState(0);
     const [tentativas, setTentativas] = useState(3);
+    const [letras_usadas, setLetras_usadas] = useState([]);
 
     console.log("A Palavra é: " + palavra + " e a categoria é: " + categoria);
 
@@ -33,31 +34,37 @@ const Jogo = () => {
         let input = document.getElementById('LetraEscolhida');
         let letra_escolhida = input.value;
         let n_letra_escolhida = letra_escolhida.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-        let palavra_na_tela_aux = palavra_na_tela.slice();
-        let achou = false;
 
         console.log("A letra escolhida foi: " + n_letra_escolhida);
         
+        if (letras_usadas.includes(n_letra_escolhida) === false && n_letra_escolhida !== '') {
+            setLetras_usadas([...letras_usadas, n_letra_escolhida]);         
 
-        palavra_array.map((letra, index) => {
-            console.log('Letra: ' + letra + ' na posição ' + index);
-            let n_letra = letra.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-            if (n_letra === n_letra_escolhida) {
-                achou = true;
-                console.log('Letra existe na posição ' + index);
-                palavra_na_tela_aux[index] = letra;
-                console.log(palavra_na_tela_aux);
-                setPalavra_na_tela(palavra_na_tela_aux);
-            }
-        })
+            let palavra_na_tela_aux = palavra_na_tela.slice();
+            let achou = false;
 
-        
+            palavra_array.map((letra, index) => {
+                console.log('Letra: ' + letra + ' na posição ' + index);
+                let n_letra = letra.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                if (n_letra === n_letra_escolhida) {
+                    achou = true;
+                    console.log('Letra existe na posição ' + index);
+                    palavra_na_tela_aux[index] = letra;
+                    console.log(palavra_na_tela_aux);
+                    setPalavra_na_tela(palavra_na_tela_aux);
+                }
+            })
 
-        if (achou === false) {
-            console.log('Letra não existe na palavra!');
-            setTentativas(tentativas - 1);            
+             
+
+            if (achou === false) {
+                console.log('Letra não existe na palavra!');
+                setTentativas(tentativas - 1);
+            }    
+        } else {
+            console.log('Letra já foi usada!');
         }
-        
+
         input.value = '';
         input.focus();
     }
@@ -77,6 +84,8 @@ const Jogo = () => {
                 <input id='LetraEscolhida' className='LetraEscolhida' type='text' maxLength='1' />
                 <button>Enviar</button>
             </form>
+            <p>Letras já escolhidas: </p>
+            <p>{letras_usadas.join(', ')}</p>
         </>
     )
 }
